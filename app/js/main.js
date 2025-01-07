1383088feb2e4a586ad92db34ae755a9b71a267b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-01-06 20:16:26 -0500

    projects section styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 const swiper = new Swiper(".projects__slider", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
